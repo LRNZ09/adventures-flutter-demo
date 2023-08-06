37a371550d7acdebb08133b2652041e24b2da4e2
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-08-06 17:08:14 +0200

    chore: project structure

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:senior_flutter_challenge/widgets/home_page/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Adventures in Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
