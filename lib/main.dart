5ec05c157027ae259be6ee4d68c687a63ad3fad6
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-08-06 17:47:56 +0200

    feat: config cached query

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:cached_query_flutter/cached_query_flutter.dart';
+import 'package:cached_storage/cached_storage.dart';
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  CachedQuery.instance.configFlutter(
+    config: QueryConfigFlutter(
+      refetchDuration: const Duration(seconds: 60),
+    ),
+    storage: await CachedStorage.ensureInitialized(),
+  );
+
   runApp(const App());
 }
